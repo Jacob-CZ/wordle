137af0f22e2a1538e7fab2c844be6dea7b3176dc
kuba <hladikjakubis@gmail.com>
2024-06-03 14:41:12 +0200

    fix: correct page

--- app/correct/page.tsx
+++ app/correct/page.tsx
@@ -1,10 +1,8 @@
 "use client"
 import React from 'react'
-import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
-export default () => {
-  const { width, height } = useWindowSize()
+export default function Page() {
   return (
     <main className=' h-screen w-screen overflow-hidden'>
     <Confetti

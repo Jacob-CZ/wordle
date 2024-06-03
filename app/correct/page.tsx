afe3b8cd71ae2952da53384e661c3bcb62eac435
kuba <hladikjakubis@gmail.com>
2024-06-03 14:46:06 +0200

    fix build error

--- app/correct/page.tsx
+++ app/correct/page.tsx
@@ -1,15 +1,33 @@
 "use client"
-import Confetti from 'react-confetti'
+import React, { useEffect, useState } from 'react';
+import Confetti from 'react-confetti';
 
 export default function Page() {
-  const { innerWidth: width, innerHeight: height } = window;
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <main className=' h-screen w-screen overflow-hidden'>
+    <main className='h-screen w-screen overflow-hidden'>
       <Confetti
         width={width}
         height={height}
       />
     </main>
-  )
+  );
 }

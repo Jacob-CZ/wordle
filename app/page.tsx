d6a6bafe0b7af548cf2389605eee3fb8807cdfb8
Kubatron <hladikjakubis@gmail.com>
2025-01-08 14:59:24 +0100

    fix shit

--- app/page.tsx
+++ app/page.tsx
@@ -1,118 +1,204 @@
 "use client"
 
-import Square from "@/components/square";
-import Warn from "@/components/warn";
-import { time } from "console";
-import Cookies, { set } from "js-cookie";
-import { cookies } from "next/headers";
-import { useRouter } from "next/navigation";
-import { KeyboardEvent, ChangeEvent, ChangeEventHandler, EventHandler, useState, useRef, useEffect, use } from "react";
-import { TypeAnimation } from "react-type-animation";
+import Square from "@/components/square"
+import Warn from "@/components/warn"
+import { time } from "console"
+import Cookies, { set } from "js-cookie"
+import { useRouter } from "next/navigation"
+import {
+	KeyboardEvent,
+	ChangeEvent,
+	ChangeEventHandler,
+	EventHandler,
+	useState,
+	useRef,
+	useEffect,
+} from "react"
+import { TypeAnimation } from "react-type-animation"
 const secretWord = "HELLO"
 export default function Home() {
-  const [word, setWord] = useState<Array<Array<string>>>(Array.from({length:6}).map(() => (["","","","",""])))
-  const [letterStatus, setLetterStatus] = useState<Array<Array<"none" | "correct" | "contains" | "incorrect">>>(Array.from({length:6}).map(() => (["none","none","none","none","none"])))
-  const [column, setColumn] = useState(0)
-  const [row, setRow] = useState(0)
-  const [finished, setFinished] = useState(true)
-  const [shake, setShake] = useState(-1)
-  const [startTimestamp, setStartTimestamp] = useState<number>(0)
-  const router = useRouter()
-  useEffect(() => {
-    const startTimestamp = Number(Cookies.get('time')) || new Date().getTime()
-    setStartTimestamp(startTimestamp)
-    const now = new Date();
-    const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-    const expiresIn = (endOfDay.getTime() - now.getTime()) / 1000; // Convert to seconds
-    if (Cookies.get('Today') === 'true') {
-      Cookies.set('Today','true', { expires: expiresIn / (60 * 60 * 24) })
-      setFinished(true)
-    }else{
-      setFinished(false)
-    }
-    if(Cookies.get('row') && Cookies.get('rowData') && Cookies.get('letterStatus')){
-      setRow(Number(Cookies.get('row')))
-      setWord(JSON.parse(Cookies.get('rowData')!))
-      setLetterStatus(JSON.parse(Cookies.get('letterStatus')!))
-    }
-    const interval = setInterval(() => {
-      if (!startTimestamp) return
-      const currentTimestamp = new Date().getTime()
-      const elapsed = new Date(currentTimestamp - startTimestamp)
-      Cookies.set('time', elapsed.getTime().toString(),{ expires : expiresIn / (60 * 60 * 24)})
-    }, 0)
-    return () => clearInterval(interval)
-  },[])
-  const submit = () => {
+	const [word, setWord] = useState<Array<Array<string>>>(
+		Array.from({ length: 6 }).map(() => ["", "", "", "", ""])
+	)
+	const [letterStatus, setLetterStatus] = useState<
+		Array<Array<"none" | "correct" | "contains" | "incorrect">>
+	>(
+		Array.from({ length: 6 }).map(() => [
+			"none",
+			"none",
+			"none",
+			"none",
+			"none",
+		])
+	)
+	const [column, setColumn] = useState(0)
+	const [row, setRow] = useState(0)
+	const [finished, setFinished] = useState(true)
+	const [shake, setShake] = useState(-1)
+	const [startTimestamp, setStartTimestamp] = useState<number>(0)
+	const router = useRouter()
+	useEffect(() => {
+		const startTimestamp =
+			Number(Cookies.get("time")) || new Date().getTime()
+		setStartTimestamp(startTimestamp)
+		const now = new Date()
+		const endOfDay = new Date(
+			now.getFullYear(),
+			now.getMonth(),
+			now.getDate() + 1,
+			0,
+			0,
+			0
+		)
+		const expiresIn = (endOfDay.getTime() - now.getTime()) / 1000 // Convert to seconds
+		if (Cookies.get("Today") === "true") {
+			Cookies.set("Today", "true", {
+				expires: expiresIn / (60 * 60 * 24),
+			})
+			setFinished(true)
+		} else {
+			setFinished(false)
+		}
+		if (
+			Cookies.get("row") &&
+			Cookies.get("rowData") &&
+			Cookies.get("letterStatus")
+		) {
+			setRow(Number(Cookies.get("row")))
+			setWord(JSON.parse(Cookies.get("rowData")!))
+			setLetterStatus(JSON.parse(Cookies.get("letterStatus")!))
+		}
+		const interval = setInterval(() => {
+			if (!startTimestamp) return
+			const currentTimestamp = new Date().getTime()
+			const elapsed = new Date(currentTimestamp - startTimestamp)
+			Cookies.set("time", elapsed.getTime().toString(), {
+				expires: expiresIn / (60 * 60 * 24),
+			})
+		}, 0)
+		return () => clearInterval(interval)
+	}, [])
+	const submit = () => {
+		if (finished) {
+			return
+		}
+		const currentTimestamp = new Date().getTime()
+		const elapsed = new Date(currentTimestamp - startTimestamp)
+		fetch("/api/validate_word", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ word: word[row], time: elapsed.getTime() }),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (!data.word) {
+					setShake(row)
+					setColumn(0)
+					setWord(
+						word.map((l, i) =>
+							i === row ? ["", "", "", "", ""] : l
+						)
+					)
+					return
+				}
+				if (
+					data.word.every(
+						(l: "none" | "correct" | "contains" | "incorrect") =>
+							l === "correct"
+					)
+				) {
+					router.push(`/correct`)
+				}
+				console.log(data.word)
+				setLetterStatus(
+					letterStatus.map((l, i) => (i === row ? data.word : l))
+				)
+				setRow(row + 1)
+				setColumn(0)
+				const now = new Date()
+				const endOfDay = new Date(
+					now.getFullYear(),
+					now.getMonth(),
+					now.getDate() + 1,
+					0,
+					0,
+					0
+				)
+				const expiresIn = (endOfDay.getTime() - now.getTime()) / 1000 // Convert to seconds
+				Cookies.set("row", (row + 1).toString(), {
+					expires: expiresIn / (60 * 60 * 24),
+				})
+				Cookies.set("rowData", JSON.stringify(word), {
+					expires: expiresIn / (60 * 60 * 24),
+				})
+				Cookies.set(
+					"letterStatus",
+					JSON.stringify(
+						letterStatus.map((l, i) => (i === row ? data.word : l))
+					),
+					{ expires: expiresIn / (60 * 60 * 24) }
+				)
+				if (row === 5) {
+					setFinished(true)
+					const now = new Date()
+					const endOfDay = new Date(
+						now.getFullYear(),
+						now.getMonth(),
+						now.getDate() + 1,
+						0,
+						0,
+						0
+					)
+					const expiresIn =
+						(endOfDay.getTime() - now.getTime()) / 1000 // Convert to seconds
 
-    if (finished) {
-      return;
-    }
-    const currentTimestamp = new Date().getTime()
-    const elapsed = new Date(currentTimestamp - startTimestamp)
-    fetch("/api/validate_word", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({word: word[row], time : elapsed.getTime()})
-    }).then(res => res.json()).then(data => {
-      if (!data.word){
-        setShake(row)
-        setColumn(0)
-        setWord(word.map((l, i) => i === row ? ["","","","",""] : l))
-        return 
-      }
-      if (data.word.every((l: "none" | "correct" | "contains" | "incorrect") => l === "correct")) {
-        router.push(`/correct`)
-      }
-      console.log(data.word)
-      setLetterStatus(letterStatus.map((l, i) => i === row ? data.word : l))
-      setRow(row + 1)
-      setColumn(0)
-      const now = new Date();
-      const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-      const expiresIn = (endOfDay.getTime() - now.getTime()) / 1000; // Convert to seconds
-      Cookies.set('row', (row + 1).toString(), { expires: expiresIn / (60 * 60 * 24) })
-      Cookies.set('rowData', JSON.stringify(word), { expires : expiresIn / (60 * 60 * 24)})
-      Cookies.set('letterStatus', JSON.stringify(letterStatus.map((l, i) => i === row ? data.word : l)), { expires : expiresIn / (60 * 60 * 24)})
-      if (row === 5) {
-        setFinished(true)
-        const now = new Date();
-        const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-        const expiresIn = (endOfDay.getTime() - now.getTime()) / 1000; // Convert to seconds
-      
-        // Set the cookie to expire at the end of the day
-        Cookies.set('Today', 'true', { expires: expiresIn / (60 * 60 * 24) }); // Convert seconds to days for js-cookie
-        window.location.reload()
-      }
-    })
+					// Set the cookie to expire at the end of the day
+					Cookies.set("Today", "true", {
+						expires: expiresIn / (60 * 60 * 24),
+					}) // Convert seconds to days for js-cookie
+					window.location.reload()
+				}
+			})
+	}
 
-  }
-
-  return (
-    <main className="flex flex-col items-center justify-center h-screen" >
-      <TypeAnimation
-        sequence={[
-          // Same substring at the start will only be typed out once, initially
-          'Wordle',
-          1000, // wait 1s before replacing "Mice" with "Hamsters"
-          'Delta Wordle',
-          1000,
-        ]}
-        wrapper="span"
-        speed={20}
-        className="barcode text-9xl text-red-600 mb-5"
-        repeat={Infinity}
-      />
-      <div className=" grid grid-cols-5 grid-rows-6 gap-4">
-        {Array.from({ length: 6 }).map((_, i) => (
-          Array.from({ length: 5 }).map((_, j) => (
-          <Square shake={shake} setshake={setShake} state={letterStatus[i][j]} word={word} setWord={setWord} setColumn={setColumn} key={i*5 + j} row={i} column={j} currentRow={row} currentColumn={column} submit={submit} />
-          ))
-        ))}
-      </div>
-      {finished && <Warn />}
-    </main>
-  );
+	return (
+		<main className="flex flex-col items-center justify-center h-screen p-4">
+			<TypeAnimation
+				sequence={[
+					// Same substring at the start will only be typed out once, initially
+					"Wordle",
+					1000, // wait 1s before replacing "Mice" with "Hamsters"
+					"Delta Wordle",
+					1000,
+				]}
+				wrapper="span"
+				speed={20}
+				className="barcode text-5xl text-red-600 mb-5 sm:text-6xl md:text-7xl"
+				repeat={Infinity}
+			/>
+			<div className="grid grid-cols-5 grid-rows-6 gap-2 sm:gap-4">
+				{Array.from({ length: 6 }).map((_, i) =>
+					Array.from({ length: 5 }).map((_, j) => (
+						<Square
+							shake={shake}
+							setshake={setShake}
+							state={letterStatus[i][j]}
+							word={word}
+							setWord={setWord}
+							setColumn={setColumn}
+							key={i * 5 + j}
+							row={i}
+							column={j}
+							currentRow={row}
+							currentColumn={column}
+							submit={submit}
+						/>
+					))
+				)}
+			</div>
+			{finished && <Warn />}
+		</main>
+	)
 }
--- components/square.tsx
+++ components/square.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, KeyboardEvent, ChangeEvent, use, useState } from "react"
+import { useEffect, useRef, KeyboardEvent, ChangeEvent, useState } from "react"
 import { animate, motion, useAnimate, useMotionValue } from "framer-motion"
 export default function Square(props: {
 	state: "none" | "correct" | "contains" | "incorrect"
@@ -20,7 +20,7 @@ export default function Square(props: {
 	const height = useMotionValue("0%")
 	const [color, setColor] = useState("#2a9d8f")
 	const [rotate, setRotate] = useState(0)
-	
+
 	const shake = async () => {
 		await animate(x, 100, { duration: 0.1, ease: "easeInOut" })
 		await animate(x, 0, { duration: 0.1, ease: "easeInOut" })
@@ -34,7 +34,7 @@ export default function Square(props: {
 		if (props.shake === props.row) {
 			shake()
 		}
-	}, [props.shake]);
+	}, [props.shake])
 	useEffect(() => {
 		if (
 			props.column === props.currentColumn &&
@@ -53,18 +53,42 @@ export default function Square(props: {
 	}, [props.currentColumn, props.currentRow])
 	useEffect(() => {
 		if (props.state === "correct") {
-			animate(height, "100%", { duration: 0.1, ease: "easeInOut", delay: props.column * 0.1 })
-			animate(rorateX, 180, { duration: 1, ease: "easeInOut", delay: props.column * 0.1 })
+			animate(height, "100%", {
+				duration: 0.1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
+			animate(rorateX, 180, {
+				duration: 1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
 			setTimeout(() => setRotate(180), 500 + props.column * 100)
 			setColor("#a7c957")
 		} else if (props.state === "incorrect") {
-			animate(height, "100%", { duration: 0.1, ease: "easeInOut", delay: props.column * 0.1 })
-			animate(rorateX, 180, { duration: 1, ease: "easeInOut", delay: props.column * 0.1 })
+			animate(height, "100%", {
+				duration: 0.1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
+			animate(rorateX, 180, {
+				duration: 1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
 			setTimeout(() => setRotate(180), 500 + props.column * 100)
 			setColor("#DC2626")
 		} else if (props.state === "contains") {
-			animate(height, "100%", { duration: 0.1, ease: "easeInOut", delay: props.column * 0.1 })
-			animate(rorateX, 180, { duration: 1, ease: "easeInOut", delay: props.column * 0.1 })
+			animate(height, "100%", {
+				duration: 0.1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
+			animate(rorateX, 180, {
+				duration: 1,
+				ease: "easeInOut",
+				delay: props.column * 0.1,
+			})
 			setTimeout(() => setRotate(180), 500 + props.column * 100)
 			setColor("#fcbf49")
 		} else {
@@ -120,30 +144,44 @@ export default function Square(props: {
 		//bild pls
 	}
 	const handleblur = () => {
-		if(props.currentColumn === props.column && props.currentRow === props.row){
-		inputRef.current?.focus()
-	}
+		if (
+			props.currentColumn === props.column &&
+			props.currentRow === props.row
+		) {
+			inputRef.current?.focus()
+		}
 	}
 
 	return (
-		<motion.div className="relative square" style={{rotateX: rorateX, transformStyle: "preserve-3d", perspective: 800, rotateZ: rotate, rotateY: rotate}}>
+		<motion.div
+			className="relative square"
+			style={{
+				rotateX: rorateX,
+				transformStyle: "preserve-3d",
+				perspective: 800,
+				rotateZ: rotate,
+				rotateY: rotate,
+			}}
+		>
 			<motion.input
 				ref={inputRef}
 				onBlur={handleblur}
 				onKeyDown={handleKeyDown}
 				onChange={handlechange}
-				className=" pointer-events-none relative bg-neutral-800 border border-neutral-600 outline-none focus:border-white transition-all w-24 h-24 text-[5rem] text-center z-10"
+				className="pointer-events-none relative bg-neutral-800 border border-neutral-600 outline-none focus:border-white transition-all w-10 h-10 text-3xl text-center z-10 sm:w-12 sm:h-12 sm:text-5xl md:w-14 md:h-14 md:text-4xl"
 				type="text"
 				style={{
 					y: y,
-					x: x
+					x: x,
 				}}
 			/>
-			<motion.div style={{
-				height: height, backgroundColor: color
-			}} className="  w-full absolute top-0 left-0">
-
-			</motion.div>
+			<motion.div
+				style={{
+					height: height,
+					backgroundColor: color,
+				}}
+				className="w-full absolute top-0 left-0"
+			></motion.div>
 		</motion.div>
 	)
 }
